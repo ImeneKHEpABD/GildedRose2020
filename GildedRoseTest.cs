10f6363cc93ea42c1ab045850e413b9fed1576a3
ImeneKHEpABD <khemiriimene@gmail.com>
2020-03-28 14:52:57 +0100

    Add the Should_Return_Zero_Quality_When_Current_Quality_Is_Equal_To_Zero_And_Item_Is_Not_AgedBrie_And_Not_Backstage_Passes unit Test

--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -14,5 +14,15 @@ namespace csharp
             app.UpdateQuality();
             Assert.AreEqual("foo", Items[0].Name);
         }
+        [Test]
+        public void Should_Return_Zero_Quality_When_Current_Quality_Is_Equal_To_Zero_And_Item_Is_Not_AgedBrie_And_Not_Backstage_Passes()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 1, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(0, Items[0].Quality
+                
+                );
+        }
     }
 }
